84b630a4aec9be72e95bc4b36ca5a9d1d7970af3
ginoliza <ginoangelolr@gmail.com>
2023-03-04 22:48:17 -0500

    Seccion 13 en desarrollo 8/15

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,11 +1,38 @@
 // Obtener el tamaño del array de botones 
 var len = document.querySelectorAll(".drum").length;
 
-function gotClicked(){
-    alert("I got clicked");
+function playSound() {
+    var foo = this.innerHTML;
+    var audio = new Audio("sounds/tom-1.mp3");
+    switch (foo) {
+        case "w":
+            audio = new Audio("sounds/tom-1.mp3");
+            break;
+        case "a":
+            audio = new Audio("sounds/tom-2.mp3");
+            break;
+        case "s":
+            audio = new Audio("sounds/tom-3.mp3");
+            break;
+        case "d":
+            audio = new Audio("sounds/tom-4.mp3");
+            break;
+        case "j":
+            audio = new Audio("sounds/snare.mp3");
+            break;
+        case "k":
+            audio = new Audio("sounds/crash.mp3");
+            break;
+        case "l":
+            audio = new Audio("sounds/kick-bass.mp3");
+            break;
+        default:
+            break;
+    }
+    audio.play();
 }
 
 // Iterar por todo el array de botones
-for(var i=0;i<len;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", gotClicked);
+for (var i = 0; i < len; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
 }
