087f985376792b5f61cdfe56816ec857cc0488bd
ginoliza <ginoangelolr@gmail.com>
2023-03-05 23:54:26 -0500

    Seccion 13 terminada, 14 en desarrollo 5/11

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,38 +1,61 @@
+// CLICKS
 // Obtener el tamaño del array de botones 
 var len = document.querySelectorAll(".drum").length;
 
-function playSound() {
-    var foo = this.innerHTML;
-    var audio = new Audio("sounds/tom-1.mp3");
-    switch (foo) {
+// Iterar por todo el array de botones
+for (var i = 0; i < len; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        makeSound(this.innerHTML);
+        buttonAnimation(event.key);
+    });
+}
+
+// TECLADO
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+// FUNCION 
+function makeSound(key) {
+    switch (key) {
         case "w":
             audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
             break;
         case "a":
             audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
             break;
         case "s":
             audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
             break;
         case "d":
             audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
             break;
         case "j":
             audio = new Audio("sounds/snare.mp3");
+            audio.play();
             break;
         case "k":
             audio = new Audio("sounds/crash.mp3");
+            audio.play();
             break;
         case "l":
             audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
             break;
         default:
             break;
     }
-    audio.play();
 }
 
-// Iterar por todo el array de botones
-for (var i = 0; i < len; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
